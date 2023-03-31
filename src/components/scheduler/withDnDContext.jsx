a9589e6518b909e1aafea2a7645357436b8fc08a
avichovatiya67 <avinash.chovatiya@thegatewaycorp.co.in>
2023-03-31 17:19:29 +0530

    DND 1

--- src/components/scheduler/withDnDContext.jsx
+++ src/components/scheduler/withDnDContext.jsx
@@ -1,4 +1,4 @@
 import { DragDropContext } from "react-dnd";
-import createHTML5Backend from "react-dnd-html5-backend";
+import HTML5Backend from "react-dnd-html5-backend";
 
-export default DragDropContext(createHTML5Backend);
+export default DragDropContext(HTML5Backend);
